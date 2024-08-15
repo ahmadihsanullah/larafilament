8b7441c9b179ce0f233e1db02b06b0d1c1ce9346
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-08-15 10:25:13 +0700

    menambah badge dan daterangepicker

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -10,13 +10,17 @@ use App\Models\Category;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 use Filament\Resources\Resource;
 use Filament\Forms\Components\Card;
+use Filament\Tables\Filters\Filter;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Toggle;
+use Filament\Tables\Filters\Indicator;
 use Filament\Tables\Columns\TextColumn;
 use App\Http\Controllers\PostController;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\RichEditor;
 use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Filters\SelectFilter;
@@ -26,9 +30,10 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\PostResource\RelationManagers;
 use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use App\Filament\Resources\PostResource\Widgets\StatsOverview;
+use Malzariey\FilamentDaterangepickerFilter\Filters\DateRangeFilter;
 use App\Filament\Resources\PostResource\RelationManagers\TagsRelationManager;
 use App\Filament\Resources\CategoryResource\RelationManagers\PostsRelationManager;
-use App\Filament\Resources\PostResource\Widgets\StatsOverview;
 
 class PostResource extends Resource
 {
@@ -36,9 +41,17 @@ class PostResource extends Resource
 
     protected static ?string $recordTitleAttribute = 'title';
 
-
-
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::where('status', true)->count();
+    }
+
+    public static function getNavigationBadgeColor(): ?string
+    {
+        return static::getModel()::where('status', true)->count() < 3 ? 'danger' : 'green';
+    }
 
     public static function form(Form $form): Form
     {
@@ -64,8 +77,8 @@ class PostResource extends Resource
                             ->required()
                             ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
                         TextInput::make('slug')
-                                ->unique()
-                                ->required(),
+                            ->unique()
+                            ->required(),
                         RichEditor::make('content')
                             ->required(),
                         Toggle::make('status'),
@@ -88,13 +101,46 @@ class PostResource extends Resource
                     ->options([
                         Category::all()->pluck('name', 'id')->toArray()
                     ]),
+                DateRangeFilter::make('created_at')
+                    ->withIndicator(),
+                // Filter::make('created_at')
+                // ->form([
+                //     DatePicker::make('from'),
+                //     DatePicker::make('until'),
+                // ])
+                // ->query(function (Builder $query, array $data): Builder {
+                //     return $query
+                //         ->when(
+                //             $data['from'],
+                //             fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                //         )
+                //         ->when(
+                //             $data['until'],
+                //             fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                //         );
+                // })
+                // ->indicateUsing(function (array $data): array {
+                //     $indicators = [];
+
+                //     if ($data['from'] ?? null) {
+                //         $indicators[] = Indicator::make('Created from ' . Carbon::parse($data['from'])->toFormattedDateString())
+                //             ->removeField('from');
+                //     }
+
+                //     if ($data['until'] ?? null) {
+                //         $indicators[] = Indicator::make('Created until ' . Carbon::parse($data['until'])->toFormattedDateString())
+                //             ->removeField('until');
+                //     }
+
+                //     return $indicators;
+                // }),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\Action::make('download')
                     ->color('info')
                     ->icon('heroicon-o-arrow-down-tray')
-                    ->url(fn (Post $post) => route('download.cover', $post))
+                    ->url(fn(Post $post) => route('download.cover', $post))
                     ->openUrlInNewTab()
             ])
             ->bulkActions([
@@ -127,6 +173,4 @@ class PostResource extends Resource
             StatsOverview::class,
         ];
     }
-
-    
 }
--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,11 +2,97 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Textarea;
+use App\Filament\Resources\UserResource;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard\Step;
+use Filament\Notifications\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
+
+    use CreateRecord\Concerns\HasWizard;
+
     protected static string $resource = UserResource::class;
+
+    protected function getSteps(): array
+    {
+
+        return  [
+            Wizard\Step::make('Register')
+                ->icon('heroicon-o-key')
+                ->schema([
+                    Card::make()
+                        ->schema([
+                            TextInput::make('name')
+                                ->label('Name')
+                                ->required()
+                                ->maxLength(255),
+
+                            TextInput::make('email')
+                                ->label('Email')
+                                ->email()
+                                ->required()
+                                ->maxLength(255),
+
+                            Grid::make(2)
+                                ->schema([
+                                    TextInput::make('password')
+                                        ->label('Password')
+                                        ->password()
+                                        ->dehydrateStateUsing(fn($state) => !empty($state) ? bcrypt($state) : null)
+                                        ->maxLength(255)
+                                        ->same('password_confirmation')
+                                        ->required(),
+
+                                    TextInput::make('password_confirmation')
+                                        ->label('Confirm Password')
+                                        ->password()
+                                        ->required(fn($livewire) => $livewire instanceof CreateUser)
+                                        ->maxLength(255)
+                                        ->dehydrated(false),
+                                ]),
+                            Select::make('role')
+                                ->relationship('roles', 'name')
+                                ->multiple()
+                                ->preload()   
+                                ->createOptionForm([
+                                    TextInput::make('name')
+                                    ->required()
+                                    ->unique(ignoreRecord: true)
+                                ])
+
+                        ]),
+                ]),
+            Wizard\Step::make('Profile')
+                ->icon('heroicon-o-user')
+                ->schema([
+                    TextInput::make('birth')
+                        ->type('date'),
+                    Select::make('gender')
+                        ->options([
+                            'Male' => 'Male',
+                            'Female' => 'Female'
+                        ]),
+                    Textarea::make('address')
+                        ->label('Address')
+                        ->columnSpan(2)
+                ])->columns(2),
+            Wizard\Step::make('Biodata')
+                ->icon('heroicon-o-pencil')
+                ->schema([
+                    Textarea::make('biodata')
+                        ->label('Biodata')
+                        ->rows(6)
+                ]),
+
+
+        ];
+    }
 }
